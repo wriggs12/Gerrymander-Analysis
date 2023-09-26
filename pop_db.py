1f6c3662be26ed3dfe693ff99445d008ae5772cd
wriggs12 <wriggs12@gmail.com>
2023-09-26 19:32:05 -0400

    Fix Mongo Format

--- pop_db.py
+++ pop_db.py
@@ -1,10 +1,12 @@
 import pymongo
 import geopandas
+import json
 
 cluster = pymongo.MongoClient('mongodb+srv://winston:Ybq0Pn7jVIK1jf69@cluster0.famlsic.mongodb.net/')
 
 ga_cd = geopandas.read_file('./Georgia/ga_congressional_districts.zip')
 ga_cd = ga_cd.to_json()
+ga_cd = json.loads(ga_cd)
 
 db = cluster['georgia']
 collection = db['districts']
@@ -12,6 +14,7 @@ collection.insert_one({'2022_districts': ga_cd})
 
 mi_cd = geopandas.read_file('./Michigan/mi_congressional_districts.zip')
 mi_cd = mi_cd.to_json()
+mi_cd = json.loads(mi_cd)
 
 db = cluster['michigan']
 collection = db['districts']
@@ -19,7 +22,8 @@ collection.insert_one({'2022_districts': mi_cd})
 
 nv_sa = geopandas.read_file('./Nevada/NV_SA_Districts.zip')
 nv_sa = nv_sa.to_json()
+nv_sa = json.loads(nv_sa)
 
 db = cluster['nevada']
 collection = db['districts']
-collection.insert_one({'2022_districts': nv_sa})
+collection.insert_one(nv_sa)
