815162a1b581b79c772ab3fd774acfa58f983a3c
wriggs12 <wriggs12@gmail.com>
2023-10-24 12:57:46 -0400

    Stuffff

--- Scripts/preprocess.py
+++ Scripts/preprocess.py
@@ -4,38 +4,38 @@ import json
 import utils
 import matplotlib.pyplot as plt
 import csv
+import numpy as np
 
-# from numba import njit
+def preprocess(precincts, populations):
+    precincts['NEIGHBORS'] = None
 
-def is_in_state(precincts, state):
-    for i in range(len(precincts)):
-        if (not state.contains(precincts[i], align=True).bool()):
-            precincts.drop(i)
-    return precincts
+    for index, precinct in precincts.iterrows():
+        neighbors = np.array(precincts[precincts.geometry.touches(precinct['geometry'])].GEOID)
+        precincts.at[index, 'NEIGHBORS'] = ', '.join(neighbors)
 
-def get_nevada_districts():
-    state = geopandas.read_file(utils.NEVADA_PATH + 'nv_state.zip')
-    precincts = geopandas.read_file(utils.NEVADA_PATH + 'precincts-with-results.zip')
+    print(precincts.columns)
+    print(precincts['GEOID10'])
 
-    print(state.geometry)
-    print(precincts.geometry)
+def get_nevada_data():
+    precincts = geopandas.read_file(utils.NEVADA_PATH + 'tl_rd22_32_bg.zip')
+    populations = pd.read_csv(utils.NEVADA_PATH + 'NV_POPULATION_DATA.csv')
 
-    state = state.to_crs(precincts.crs)
+    preprocess(precincts, populations)
+    
+    # print(districts.columns)
+    # print(precincts)
 
-    print(state.crs)
-    print(precincts.crs)
+    # precincts.plot()
+    # plt.show()
 
-    precincts = is_in_state(precincts.geometry, state.geometry)
-
-    print(precincts)
-
-    precincts.to_file('output.shp')
+    # districts.plot()
+    # plt.show()
 
 
-# get_nevada_districts()
 
 if __name__ == '__main__':
-    get_nevada_districts()
+    get_nevada_data()
+
 # precincts.plot()
 
 # precincts.merge(districts, how='inner')
