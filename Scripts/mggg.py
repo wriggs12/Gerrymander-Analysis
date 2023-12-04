74e78f944d3e13874669a8d67004a1aa5fb6cde4
wriggs12 <wriggs12@gmail.com>
2023-12-04 18:38:37 -0500

    Progress 1.1

--- Scripts/mggg.py
+++ Scripts/mggg.py
@@ -4,20 +4,19 @@ from gerrychain.proposals import recom
 from functools import partial
 from gerrychain.random import random
 from gerrychain.constraints import no_vanishing_districts
-import multiprocessing as mp
 
 ENSEMBLE_SIZE=1
+stats = {'DEM_DIST': [], 'REP_DIST': [], 'AA_POP': [], 'AI_POP': [], 'WH_POP': [], 'HI_POP': []}
 
 def generate_plan(chain, seed):
     random.seed(seed)
     for partition in chain.with_progress_bar():
         pass
     
-    generated_plan = partition
-    calc_party_split(generated_plan)
-    calc_race_demographics(generated_plan)
+    calc_party_split(partition)
+    calc_race_demographics(partition)
 
-    return partition
+    return (partition)
 
 def init_chain(graph):
     elections = [
@@ -58,27 +57,46 @@ def init_chain(graph):
 
     return chain
 
-def run(data):
+def run(data, dist_measure):
     graph = Graph.from_geodataframe(data)
     chain = init_chain(graph)
 
     ensemble = [generate_plan(chain, seed) for seed in range(ENSEMBLE_SIZE)]
-    for plan in ensemble:
-        print(plan.graph.nodes[0])
+    find_clusters(ensemble, dist_measure)
     
 
-def calc_party_split(plan):
+def calc_party_split(plan, stats):
     rep = 0
     dem = 0
-    for node in plan.graph:
-        dem = dem + plan.graph.nodes[node]['G20PREDBID']
-        rep = rep + plan.graph.nodes[node]['G20PRERTRU']
-
-    if (rep > dem):
-        return 'R'
-    else:
-        return 'D'
     
+    for node in plan.graph:
+        if plan.graph.nodes[node]['G20PREDBID'] > plan.graph.nodes[node]['G20PRERTRU']:
+            dem = dem + 1
+        else:
+            rep = rep + 1
 
-def calc_race_demographics(plan):
+    stats['DEM_DIST'].append(dem)
+    stats['REP_DIST'].append(rep)
+
+def calc_race_demographics(plan, stats):
+    hi_pop = 0
+    aa_pop = 0
+    ai_pop = 0
+    wh_pop = 0
+
+    for node in plan.graph:
+        hi_pop = hi_pop + plan.graph.nodes[node]['TAHISPANIC']
+        aa_pop = aa_pop + plan.graph.nodes[node]['TABLACKCMB']
+        ai_pop = ai_pop + plan.graph.nodes[node]['TAASIANCMB']
+        wh_pop = wh_pop + plan.graph.nodes[node]['TAWHITEALN']
+
+    stats['HI_POP'].append(hi_pop)
+    stats['AA_POP'].append(aa_pop)
+    stats['AI_POP'].append(ai_pop)
+    stats['WH_POP'].append(wh_pop)
+
+def save_stats():
+    pass
+
+def find_clusters(ensemble, dist_measure):
     pass
